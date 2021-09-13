deb3baf2dbe5ce4fb7cd8a0baad30289e8cfbb09
Vika Putri Ariyanti <35414466+Vputri@users.noreply.github.com>
2021-09-13 13:57:13 +0700

    Update settings.py

--- webbot/settings.py
+++ webbot/settings.py
@@ -80,6 +80,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,"static_files")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
